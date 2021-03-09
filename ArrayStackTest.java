93695335c6d3925e930db96ef17de0d3ac8e265c
Joseph <46443751+TheMusket@users.noreply.github.com>
2021-03-08 19:55:52 -0800

    Added JUnit tests.
    Added 5 JUnit tests for LinkedStackTest and ArrayStackTest respectively. Each test tests a different test case of sorts.

--- ArrayStackTest.java
+++ ArrayStackTest.java
@@ -1,5 +1,72 @@
+/*
+ * Student Name: Joseph Siwiecki
+ * Class Section: CS 2400.04
+ */
+
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class ArrayStackTest 
 {
-
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	/**
+	 * Basic test of evaluatePostfix.
+	 */
+	@Test
+	void postfixTestOne() 
+	{
+		ResizeableArrayStack<String> test = new ResizeableArrayStack<String>();
+		int output = test.evaluatePostfix("23*42-/56*+");
+		assertEquals(33, output);
+	}
+	
+	
+	/**
+	 * Tests postfix's ability to read the expression with spaces.
+	 */
+	@Test
+	void postfixTestTwo() 
+	{
+		ResizeableArrayStack<String> test = new ResizeableArrayStack<String>();
+		int output = test.evaluatePostfix("2 3 * 4 2 - / 5 6 * +");
+		assertEquals(33, output);
+	}
+	
+	
+	/**
+	 * Testing a different expression.
+	 */
+	@Test
+	void postfixTestThree() 
+	{
+		ResizeableArrayStack<String> test = new ResizeableArrayStack<String>();
+		int output = test.evaluatePostfix("45+39+*");
+		assertEquals(108, output);
+	}
+	
+	/**
+	 * Different expression to test with spaces.
+	 */
+	@Test
+	void postfixTestFour() 
+	{
+		ResizeableArrayStack<String> test = new ResizeableArrayStack<String>();
+		int output = test.evaluatePostfix("1 2 + 3 + 4 +");
+		assertEquals(10, output);
+	}
+	
+	/**
+	 * Testing expressions with the same numbers.
+	 */
+	@Test
+	void postfixTestFive() 
+	{
+		ResizeableArrayStack<String> test = new ResizeableArrayStack<String>();
+		int output = test.evaluatePostfix("8 7 + 8 *");
+		assertEquals(120, output);
+	}
 }
--- LinkedStackTest.java
+++ LinkedStackTest.java
@@ -1,5 +1,73 @@
+/*
+ * Student Name: Joseph Siwiecki
+ * Class Section: CS 2400.04
+ */
+
+import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class LinkedStackTest 
 {
-
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	/**
+	 * Basic test of convertToPostfix
+	 */
+	@Test
+	void infixToPostfixTestOne() 
+	{
+		LinkedStack<String> test = new LinkedStack<String>();
+		String output = test.convertToPostfix("a*b/(c-a)+d*e");
+		assertEquals("ab*ca-/de*+", output);
+	}
+	
+	/**
+	 * Testing if convertToPostfix can handle spaces.
+	 */
+	@Test
+	void infixToPostfixTestTwo() 
+	{
+		LinkedStack<String> test = new LinkedStack<String>();
+		String output = test.convertToPostfix("a * b / (c - a) + d * e");
+		assertEquals("ab*ca-/de*+", output);
+	}
+	
+	/**
+	 * Different expression, also checks if capitals work.
+	 */
+	@Test
+	void infixToPostfixTestThree() 
+	{
+		LinkedStack<String> test = new LinkedStack<String>();
+		String output = test.convertToPostfix("(A + B) * (C + D)");
+		assertEquals("AB+CD+*", output);
+	}
+	
+	/**
+	 * Different expression.
+	 */
+	@Test
+	void infixToPostfixTestFour() 
+	{
+		LinkedStack<String> test = new LinkedStack<String>();
+		String output = test.convertToPostfix("a + b + c + d");
+		assertEquals("ab+c+d+", output);
+	}
+	
+	/**
+	 * Different expression, testing if it can handle multiple variables
+	 * with the same character.
+	 */
+	@Test
+	void infixToPostfixTestFive() 
+	{
+		LinkedStack<String> test = new LinkedStack<String>();
+		String output = test.convertToPostfix("(a+b)*a");
+		assertEquals("ab+a*", output);
+	}
+	
 }
+
